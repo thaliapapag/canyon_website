593a9da2ddea8d95fb82a32d7c3349f73f1fa71b
Thalia Papageorgiou <tpapage@emory.edu>
2024-04-19 21:48:45 -0400

    all the initial files for the website

---
+++ canyon_website/src/main/java/com/canyon_website/CanyonWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.canyon_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanyonWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanyonWebsiteApplication.class, args);
+	}
+
+}
---
+++ canyon_website/src/test/java/com/canyon_website/CanyonWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canyon_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanyonWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
